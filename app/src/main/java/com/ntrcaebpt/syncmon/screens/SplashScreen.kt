07863c211a8f127e948d64e928502818d44213d4
Chunu <electromagneticwave16@gmail.com>
2026-04-01 09:37:13 +0530

    added SplashScreen.kt

---
+++ app/src/main/java/com/ntrcaebpt/syncmon/screens/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.ntrcaebpt.syncmon.screens
+
+import android.window.SplashScreen
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
